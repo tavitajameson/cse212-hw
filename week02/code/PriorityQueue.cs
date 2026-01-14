c4744b29acfb4a9055b1e3b805923e7bb92d6aa1
tavitajameson <kavikajameson@gmail.com>
2026-01-13 21:32:32 -0700

    Applied Fixes to PriorityQueue.cs

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -17,21 +17,20 @@
 
     public string Dequeue()
     {
-        if (_queue.Count == 0) // Verify the queue is not empty
-        {
+        if (_queue.Count == 0)
             throw new InvalidOperationException("The queue is empty.");
-        }
 
-        // Find the index of the item with the highest priority to remove
+        // Find the index of the item with the highest priority.
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index < _queue.Count; index++)
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
         return value;
     }
 
