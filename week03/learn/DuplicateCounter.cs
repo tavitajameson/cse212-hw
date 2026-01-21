1dd42d165e2e2cb924a83efce818e3e7855e4e3e
tavitajameson <kavikajameson@gmail.com>
2026-01-20 22:13:32 -0700

    Sets Learning Activity

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -24,7 +24,14 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        HashSet<int> seen = new HashSet<int>();
+        int duplicates = 0;
+
+        foreach (int item in data)
+            if (!seen.Add(item))
+            {
+                duplicates++;
+            }
+        return duplicates;
     }
 }
