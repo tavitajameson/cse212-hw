a8f28692ed907d11f348b50ea7656f3aaa0c8878
tavitajameson <kavikajameson@gmail.com>
2026-02-05 21:38:20 -0700

    Week 05 Recursion Activity

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -14,8 +14,12 @@ public static class Recursion
     /// </summary>
     public static int SumSquaresRecursive(int n)
     {
-        // TODO Start Problem 1
-        return 0;
+        // Base case
+        if (n <= 0)
+            return 0;
+
+        // Recursive case: n^2 + sum of squares up to (n-1)
+        return (n * n) + SumSquaresRecursive(n - 1);
     }
 
     /// <summary>
@@ -39,50 +43,27 @@ public static class Recursion
     /// </summary>
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
-        // TODO Start Problem 2
+        // Base case: built a word of desired length
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+
+        // Choose each letter, remove it from the remaining pool, recurse
+        for (int i = 0; i < letters.Length; i++)
+        {
+            char chosen = letters[i];
+            string remaining = letters.Remove(i, 1);
+            PermutationsChoose(results, remaining, size, word + chosen);
+        }
     }
 
     /// <summary>
     /// #############
     /// # Problem 3 #
     /// #############
-    /// Imagine that there was a staircase with 's' stairs.  
-    /// We want to count how many ways there are to climb 
-    /// the stairs.  If the person could only climb one 
-    /// stair at a time, then the total would be just one.  
-    /// However, if the person could choose to climb either 
-    /// one, two, or three stairs at a time (in any order), 
-    /// then the total possibilities become much more 
-    /// complicated.  If there were just three stairs,
-    /// the possible ways to climb would be four as follows:
-    ///
-    ///     1 step, 1 step, 1 step
-    ///     1 step, 2 step
-    ///     2 step, 1 step
-    ///     3 step
-    ///
-    /// With just one step to go, the ways to get
-    /// to the top of 's' stairs is to either:
-    ///
-    /// - take a single step from the second to last step, 
-    /// - take a double step from the third to last step, 
-    /// - take a triple step from the fourth to last step
-    ///
-    /// We don't need to think about scenarios like taking two 
-    /// single steps from the third to last step because this
-    /// is already part of the first scenario (taking a single
-    /// step from the second to last step).
-    ///
-    /// These final leaps give us a sum:
-    ///
-    /// CountWaysToClimb(s) = CountWaysToClimb(s-1) + 
-    ///                       CountWaysToClimb(s-2) +
-    ///                       CountWaysToClimb(s-3)
-    ///
-    /// To run this function for larger values of 's', you will need
-    /// to update this function to use memoization.  The parameter
-    /// 'remember' has already been added as an input parameter to 
-    /// the function for you to complete this task.
+    /// Count ways to climb using 1,2,3 steps, with memoization.
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
@@ -96,10 +77,21 @@ public static class Recursion
         if (s == 3)
             return 4;
 
-        // TODO Start Problem 3
+        // Initialize memo dictionary if needed
+        remember ??= new Dictionary<int, decimal>();
 
-        // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        // If already computed, return it
+        if (remember.TryGetValue(s, out decimal cached))
+            return cached;
+
+        // Compute recursively (but pass memo!)
+        decimal ways =
+            CountWaysToClimb(s - 1, remember) +
+            CountWaysToClimb(s - 2, remember) +
+            CountWaysToClimb(s - 3, remember);
+
+        // Store and return
+        remember[s] = ways;
         return ways;
     }
 
@@ -107,37 +99,75 @@ public static class Recursion
     /// #############
     /// # Problem 4 #
     /// #############
-    /// A binary string is a string consisting of just 1's and 0's.  For example, 1010111 is 
-    /// a binary string.  If we introduce a wildcard symbol * into the string, we can say that 
-    /// this is now a pattern for multiple binary strings.  For example, 101*1 could be used 
-    /// to represent 10101 and 10111.  A pattern can have more than one * wildcard.  For example, 
-    /// 1**1 would result in 4 different binary strings: 1001, 1011, 1101, and 1111.
-    ///	
-    /// Using recursion, insert all possible binary strings for a given pattern into the results list.  You might find 
-    /// some of the string functions like IndexOf and [..X] / [X..] to be useful in solving this problem.
+    /// Expand wildcard pattern (*) into all binary strings.
     /// </summary>
     public static void WildcardBinary(string pattern, List<string> results)
     {
-        // TODO Start Problem 4
+        // Find first wildcard
+        int idx = pattern.IndexOf('*');
+
+        // Base case: no wildcards left
+        if (idx == -1)
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        // Replace that wildcard with 0 and 1, recurse
+        string prefix = pattern[..idx];
+        string suffix = pattern[(idx + 1)..];
+
+        WildcardBinary(prefix + "0" + suffix, results);
+        WildcardBinary(prefix + "1" + suffix, results);
     }
 
     /// <summary>
+    /// #############
+    /// # Problem 5 #
+    /// #############
     /// Use recursion to insert all paths that start at (0,0) and end at the
     /// 'end' square into the results list.
     /// </summary>
     public static void SolveMaze(List<string> results, Maze maze, int x = 0, int y = 0, List<ValueTuple<int, int>>? currPath = null)
     {
-        // If this is the first time running the function, then we need
-        // to initialize the currPath list.
-        if (currPath == null) {
+        // Initialize path on first call
+        if (currPath == null)
             currPath = new List<ValueTuple<int, int>>();
+
+        // Add current position to path
+        currPath.Add((x, y));
+
+        // If we're at the end, store the path and backtrack
+        if (maze.IsEnd(x, y))
+        {
+            results.Add(currPath.AsString());
+            currPath.RemoveAt(currPath.Count - 1);
+            return;
         }
-        
-        // currPath.Add((1,2)); // Use this syntax to add to the current path
 
-        // TODO Start Problem 5
-        // ADD CODE HERE
+        // Explore neighbors in a consistent order:
+        // (Right, Down, Left, Up) — order doesn't matter because tests sort results,
+        // but being consistent avoids surprises.
+        var moves = new (int dx, int dy)[]
+        {
+            (1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1)
+        };
 
-        // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
+        foreach (var (dx, dy) in moves)
+        {
+            int nx = x + dx;
+            int ny = y + dy;
+
+            if (maze.IsValidMove(currPath, nx, ny))
+            {
+                SolveMaze(results, maze, nx, ny, currPath);
+            }
+        }
+
+        // Backtrack: remove current position before returning to caller
+        currPath.RemoveAt(currPath.Count - 1);
     }
 }
